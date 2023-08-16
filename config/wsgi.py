4710dd9fc08c2d56b33bc61489716815755f909c
Yehor Kosiak <yegorkosyak32@gmail.com>
2023-08-17 00:55:42 +0200

    refactor again

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for firsttutorial project.
+WSGI config for config project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "firsttutorial.settings")
+
+# This allows easy placement of apps within the interior project directory
+ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent
+sys.path.append(str(ROOT_DIR / "project"))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
--- project/beds_app/apps.py
+++ project/beds_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BedsAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "beds_app"
+    name = "project.beds_app"
